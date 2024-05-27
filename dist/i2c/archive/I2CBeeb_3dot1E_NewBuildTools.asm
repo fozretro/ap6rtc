ff5bee6522c2fe7b10e4b30f11d03b7859b1f5b1
Andrew Fawcett <106936946+fozretro@users.noreply.github.com>
2024-05-27 11:06:48 -0700

    BInary compare testing with oringnial source and binary from Martin - passed!

--- dist/i2c/archive/I2CBeeb_3dot1E_NewBuildTools.asm
+++ dist/i2c/archive/I2CBeeb_3dot1E_NewBuildTools.asm
@@ -1516,6 +1516,72 @@ txloop	TXA
 	TAX
 	RTS
 
+\-------------------------------------------------------------------------------
+\Gets time and date parameters from RTC into buffer buf00-buf07 @ $0380
+
+getrtc	LDA	#>buf00		\re-direct i2c buffer to buf00
+	STA	bufloc		\($0380)
+	LDA	#<buf00
+	STA	bufloc+1
+				\set up rxd call to fetch all RTC data
+	LDA	#RTC		\DS3231 RTC device id
+	STA	$68
+	LDA	#0		\register number - start at 0 (secs)
+	STA	$69
+	LDA	#19		\number of bytes to fetch (addr $00-$12)
+	STA	$6A
+	STA	$6C		\reg-valid flag to non-zero
+	JSR	cmd6		\and make the rxd(go) call
+
+	LDA	#>i2cbuf		\reset I2C buffer to $0A00
+	STA	bufloc
+	LDA	#<i2cbuf
+	STA	bufloc+1
+
+	RTS			\and return
+
+\------------------------------------------------------------------------------
+\Writes the 7-byte time and date data set at buf00-buf06 to the DS3231 RTC
+\First copies the 7 t&d bytes across to the main I2C buffer at $0A00 and then
+\performs the write using an internal txd call.
+
+writetd	LDX	#0		\copy t&d data to I2C buffer
+wtd_a1	LDA	buf00,X
+	STA	i2cbuf,X
+	INX
+	CPX	#7		\copying 7 bytes
+	BNE	wtd_a1
+	
+	LDA	#RTC		\set up txd call
+	STA	$68		\slave address
+	LDA	#0
+	STA	$69		\start register
+	STA	$6D		\no stop inhibit
+	LDA	#7
+	STA	$6A		\7 bytes to tx
+	STA	$6C		\non-zero = $69 register valid
+	JSR	cmd4		\perform the write via txd(go)
+
+	RTS			\and return
+
+\------------------------------------------------------------------------------
+\ writes the toggle state for *TBRK
+\ the PCF8583 has free ram, and so we can use some of that to store the toggle state
+\ in this case in reg 11h, this contains last written year and the toggle state
+\ On calling this subscrounte &6A arrives with (bit 0) 0 or 1 depending on toggle state
+\ On returning the caller expects &6A to be untouched
+
+wtbrk	
+	LDA	#RTC		\target i2c device id (here rtc)
+	STA	$68
+	STA	$6C		\$6C<>0 mean register specified in $69
+	LDA	#12		\start register = 12 = Alarm 2 Hours
+	STA	$69
+	LDA	#0
+	STA	$6D		\$6D=0 means Stop after txb
+	JSR	cmd3		\and send the byte via txb(go)
+	RTS
+
 \------------------------------------------------------------------------------
 \
 \RTC time and date functions
@@ -1889,72 +1955,6 @@ xxnow	LDX	#$FF		\this will signal no <cr> after time
 	JSR	xxtemp		\print temperature
 
 	RTS			\and return
-
-\-------------------------------------------------------------------------------
-\Gets time and date parameters from RTC into buffer buf00-buf07 @ $0380
-
-getrtc	LDA	#>buf00		\re-direct i2c buffer to buf00
-	STA	bufloc		\($0380)
-	LDA	#<buf00
-	STA	bufloc+1
-				\set up rxd call to fetch all RTC data
-	LDA	#RTC		\DS3231 RTC device id
-	STA	$68
-	LDA	#0		\register number - start at 0 (secs)
-	STA	$69
-	LDA	#19		\number of bytes to fetch (addr $00-$12)
-	STA	$6A
-	STA	$6C		\reg-valid flag to non-zero
-	JSR	cmd6		\and make the rxd(go) call
-
-	LDA	#>i2cbuf		\reset I2C buffer to $0A00
-	STA	bufloc
-	LDA	#<i2cbuf
-	STA	bufloc+1
-
-	RTS			\and return
-
-\------------------------------------------------------------------------------
-\Writes the 7-byte time and date data set at buf00-buf06 to the DS3231 RTC
-\First copies the 7 t&d bytes across to the main I2C buffer at $0A00 and then
-\performs the write using an internal txd call.
-
-writetd	LDX	#0		\copy t&d data to I2C buffer
-wtd_a1	LDA	buf00,X
-	STA	i2cbuf,X
-	INX
-	CPX	#7		\copying 7 bytes
-	BNE	wtd_a1
-	
-	LDA	#RTC		\set up txd call
-	STA	$68		\slave address
-	LDA	#0
-	STA	$69		\start register
-	STA	$6D		\no stop inhibit
-	LDA	#7
-	STA	$6A		\7 bytes to tx
-	STA	$6C		\non-zero = $69 register valid
-	JSR	cmd4		\perform the write via txd(go)
-
-	RTS			\and return
-
-\------------------------------------------------------------------------------
-\ writes the toggle state for *TBRK
-\ the PCF8583 has free ram, and so we can use some of that to store the toggle state
-\ in this case in reg 11h, this contains last written year and the toggle state
-\ On calling this subscrounte &6A arrives with (bit 0) 0 or 1 depending on toggle state
-\ On returning the caller expects &6A to be untouched
-
-wtbrk	
-	LDA	#RTC		\target i2c device id (here rtc)
-	STA	$68
-	STA	$6C		\$6C<>0 mean register specified in $69
-	LDA	#12		\start register = 12 = Alarm 2 Hours
-	STA	$69
-	LDA	#0
-	STA	$6D		\$6D=0 means Stop after txb
-	JSR	cmd3		\and send the byte via txb(go)
-	RTS
 
 \------------------------------------------------------------------------------
 \*TSET
