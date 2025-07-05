fa141e9d22904645f6bb56669dd12eab170ccc32
Andrew Fawcett <1167760+afawcett@users.noreply.github.com>
2025-07-05 16:51:23 +0100

    Fixed file name issue in ssd

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,15 @@
+# Always delete the old SSD to avoid trailing dot issues
+rm -f ./dist/i2c.ssd
 # Create ./dist/i2c.ssd using mmbutils
 ./bin/mmbutils/beeb blank_ssd ./dist/i2c.ssd
-./bin/mmbutils/beeb putfile ./dist/i2c.ssd ./dist/i2c/I2C32B.rom ./dist/i2c/I2C32E.rom ./dist/i2c/I2C32EAP6.rom
+# Create temporary files with correct names (no trailing dots)
+cp ./dist/i2c/I2C32B.rom ./dist/i2c/I2C32B
+cp ./dist/i2c/I2C32E.rom ./dist/i2c/I2C32E
+cp ./dist/i2c/I2C32EAP6.rom ./dist/i2c/I2C32EAP6
+./bin/mmbutils/beeb putfile ./dist/i2c.ssd ./dist/i2c/I2C32B ./dist/i2c/I2C32E ./dist/i2c/I2C32EAP6
 ./bin/mmbutils/beeb title ./dist/i2c.ssd i2crom
+# Clean up temporary files
+rm ./dist/i2c/I2C32B ./dist/i2c/I2C32E ./dist/i2c/I2C32EAP6
 # Expand it back out into the Beebs development workspace the OBJECT file for local running and to update .inf file for OBJECT
 rm -rf ./dev/ap6rtc-dist
 ./bin/mmbutils/beeb getfile ./dist/i2c.ssd ./dev/ap6rtc-dist
